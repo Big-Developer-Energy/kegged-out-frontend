731db70ebdaeafc193381ee72537cc35c7c67ae7
Danny De La Rosa <danny.devlarosa@gmail.com>
2024-01-11 14:40:27 -0800

    tests done on Signup

--- src/_tests_/Signup.test.js
+++ src/_tests_/Signup.test.js
@@ -1,23 +1,33 @@
-import { render, screen } from "@testing-library/react"
-import Signup from "../pages/Signup"
-import { BrowserRouter } from "react-router-dom"
+import { render, screen, container } from "@testing-library/react";
+import Signup from "../pages/Signup";
+import { BrowserRouter } from "react-router-dom";
 
 describe("<Signup />", () => {
-    const renderSignUp = () => {
-        render(
-        <BrowserRouter>
+  const renderSignUp = () => {
+    render(
+      <BrowserRouter>
         <Signup />
-        </BrowserRouter>
-        )
-    }
-    it("renders without crashing", () => {
-        renderSignUp();
-        screen.logTestingPlaygroundURL()
-    })
-})
-
-describe("handleSubmit", () => {
-    it("changes the value of an event", () => {
-        expect(handleSubmit).to(preventDefault())
-    })
-})
+      </BrowserRouter>
+    );
+  };
+  it("renders without crashing", () => {
+    renderSignUp();
+    screen.logTestingPlaygroundURL();
+  });
+  it("renders a signup form", () => {
+    renderSignUp()
+    const emailInput = screen.getByRole('textbox')
+    expect(emailInput).toBeInTheDocument()
+    const passwordInput = screen.getByText(/password/i)
+    expect(passwordInput).toBeInTheDocument()
+    const confirmPasswordInput = screen.getByText(/confirm password/i)
+    expect(confirmPasswordInput).toBeInTheDocument()
+  })
+  it("creates account on submit", () => {
+    renderSignUp()
+    const submitButton = screen.getByRole('button', {
+        name: /submit/i
+      })
+      expect(submitButton).toBeInTheDocument()
+  })
+});
