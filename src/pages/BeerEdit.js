3845bd94e73a43323c995001b3b3d681db9b2a1d
Danny De La Rosa <danny.devlarosa@gmail.com>
2024-01-17 17:47:24 -0800

    debugging

--- src/pages/BeerEdit.js
+++ src/pages/BeerEdit.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Form, FormGroup, Input, Label } from "reactstrap";
 import { useNavigate, useParams } from "react-router-dom";
 
-
-
 const BeerEdit = ({ beers, updateBeer, currentUser }) => {
-
-  const { id } = useParams()
-  console.log(currentUser)
-  let currentBeer = beers?.find((beer) => beer.id === +id)
+  const { id } = useParams();
+  console.log(currentUser);
+  let currentBeer = beers?.find((beer) => beer.id === +id);
 
   const [editBeer, setEditBeer] = useState({
     id: currentBeer?.id,
@@ -18,52 +15,76 @@ const BeerEdit = ({ beers, updateBeer, currentUser }) => {
     location: currentBeer?.location,
     image: currentBeer?.image,
     // user_id: currentUser?.id
-  })
+  });
 
   const handleChange = (e) => {
-    setEditBeer({ ...editBeer, [e.target.name]: e.target.value })
-  }
-  const navigate = useNavigate()
+    setEditBeer({ ...editBeer, [e.target.name]: e.target.value });
+  };
+  const navigate = useNavigate();
   const handleSubmit = () => {
-    updateBeer(editBeer)
-    navigate("/BeerIndex")
-  }
+    updateBeer(editBeer);
+    navigate("/BeerIndex");
+  };
 
   return (
     <>
-      {currentBeer && (
-        <>
-        <h1 className='beer-edit-title'>Edit a Brewview</h1>
-        <div className='beer-edit-form'>
+      <main>
+        <h1 className="beer-edit-title">Edit a Brewview</h1>
+        <div className="beer-edit-form">
           <Form>
-            <FormGroup className='beer-edit-form-group'>
+            <FormGroup className="beer-edit-form-group">
               <Label for="name">Beer Name</Label>
-              <Input type="text" name="name" onChange={handleChange} value={editBeer.name} />
+              <Input
+                type="text"
+                name="name"
+                onChange={handleChange}
+                value={editBeer.name}
+              />
             </FormGroup>
-            <FormGroup className='beer-edit-form-group'>
+            <FormGroup className="beer-edit-form-group">
               <Label for="rating">Rating</Label>
-              <Input type="text" name="rating" onChange={handleChange} value={editBeer.rating} />
+              <Input
+                type="text"
+                name="rating"
+                onChange={handleChange}
+                value={editBeer.rating}
+              />
             </FormGroup>
-            <FormGroup className='beer-edit-form-group'>
+            <FormGroup className="beer-edit-form-group">
               <Label for="description">Description of Beer</Label>
-              <Input type="text" name="description" onChange={handleChange} value={editBeer.description} />
+              <Input
+                type="text"
+                name="description"
+                onChange={handleChange}
+                value={editBeer.description}
+              />
             </FormGroup>
-            <FormGroup className='beer-edit-form-group'>
+            <FormGroup className="beer-edit-form-group">
               <Label for="location">Brewery Name/Location</Label>
-              <Input type="text" name="location" onChange={handleChange} value={editBeer.location} />
+              <Input
+                type="text"
+                name="location"
+                onChange={handleChange}
+                value={editBeer.location}
+              />
             </FormGroup>
-            <FormGroup className='beer-edit-form-group'>
+            <FormGroup className="beer-edit-form-group">
               <Label for="image">Upload Image</Label>
-              <Input type="text" name="image" onChange={handleChange} value={editBeer.image} />
+              <Input
+                type="text"
+                name="image"
+                onChange={handleChange}
+                value={editBeer.image}
+              />
             </FormGroup>
-          <button onClick={handleSubmit} name="submit">Submit Update</button>
+            <button onClick={handleSubmit} name="submit">
+              Submit Update
+            </button>
           </Form>
-          </div>
-        </>
-      )
-      }
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default BeerEdit
+export default BeerEdit;
